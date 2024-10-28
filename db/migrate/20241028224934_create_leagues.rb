7981d326dcde6fc7d09d8956bb0190134575605e
aucchwas <ucchwasa@gmail.com>
2024-10-28 17:50:16 -0500

    create the league model

--- db/migrate/20241028224934_create_leagues.rb
+++ db/migrate/20241028224934_create_leagues.rb
@@ -1,7 +1,7 @@
 class CreateLeagues < ActiveRecord::Migration[7.2]
   def change
     create_table :leagues do |t|
-      t.integer :id
+      t.integer :custom_id
       t.string :league_name
       t.string :type
       t.string :country_name
