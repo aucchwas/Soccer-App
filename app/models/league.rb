8a45ebaaf677f731541f6ee028aa7c642b457fae
aucchwas <ucchwasa@gmail.com>
2024-10-28 18:07:04 -0500

    added validations

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,3 +1,5 @@
 class League < ApplicationRecord
   has_many :teams
+  validates :league_name, presence: true
+  validates :country_name, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ApplicationRecord
   belongs_to :league
+  validates :team_name, presence: true
 end
