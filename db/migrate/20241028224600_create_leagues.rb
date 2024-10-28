8442dd8b5cc5f610acbaa69a9427b2e1e1714129
aucchwas <ucchwasa@gmail.com>
2024-10-28 17:46:12 -0500

    done something

---
+++ db/migrate/20241028224600_create_leagues.rb
@@ -0,0 +1,12 @@
+class CreateLeagues < ActiveRecord::Migration[7.2]
+  def change
+    create_table :leagues do |t|
+      t.integer :id
+      t.string :league_name
+      t.string :type
+      t.string :country_name
+
+      t.timestamps
+    end
+  end
+end
