cad0b1ee1b852e1498b4d487002bc4e56383f533
aucchwas <ucchwasa@gmail.com>
2024-10-29 15:07:55 -0500

    pagination added

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,10 +2,19 @@ class TeamsController < ApplicationController
   def index
     # @league = League.find(params[:league_id])
     # @teams = Team.all
-    @teams = Team.page(params[:page]).per(16)
+    if params[:query].present?
+      @teams = Team.search(params[:query]).page(params[:page]).per(5)
+    else
+      @teams = Team.page(params[:page]).per(5)
+    end
   end
 
   def show
     @team = Team.find(params[:id])
+  end
+
+  def search
+    @teams = Team.search(params[:query]).page(params[:page]).per(5)
+    render :index
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ApplicationRecord
   belongs_to :league
   validates :team_name, presence: true
+
+  def self.search(query)
+    where("team_name LIKE ?", "%#{query}%")
+  end
 end
