843f5dab6203594726e7deb42e88646afc8f59c8
aucchwas <ucchwasa@gmail.com>
2024-10-29 14:28:42 -0500

    added kaminari

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,8 @@
 class TeamsController < ApplicationController
   def index
     # @league = League.find(params[:league_id])
-    @teams = Team.all
+    # @teams = Team.all
+    @teams = Team.page(params[:page]).per(16)
   end
 
   def show
