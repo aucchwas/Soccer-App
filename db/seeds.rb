500ad8ba4a38c96c00729a8ab125956c6595a2d5
aucchwas <ucchwasa@gmail.com>
2024-10-28 19:40:58 -0500

    something was wrong with the api but superman came to rescue

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,48 @@ require 'net/http'
 require 'openssl'
 require 'json'
 
+Team.delete_all
 League.delete_all
+
+
+def get_teams(custom_id, league)
+  url = URI("https://v3.football.api-sports.io/standings?league=#{custom_id}&season=2022")
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(url)
+  request["x-rapidapi-host"] = 'v3.football.api-sports.io'
+  request["x-rapidapi-key"] = '3a1526697e73de9263945ed57a8e6040'
+
+  response = http.request(request)
+
+  if response.is_a?(Net::HTTPSuccess)
+    json_data = JSON.parse(response.read_body)
+
+    if json_data.key?('response')
+      json_data['response'].each do |item|
+        standings_data = item['league']['standings'].flatten
+
+        standings_data.each do |team|
+          team_data = team['team']
+
+          Team.find_or_create_by!(
+            team_name: team_data['name'],
+            points: team['points'],
+            goal_diff: team['goalsDiff'],
+            league_id: league.id
+          )
+        end
+      end
+    else
+      puts "Key 'response' not found in the JSON data."
+    end
+  else
+    puts "HTTP request failed with code: #{response.code}"
+  end
+end
 
 url = URI("https://v3.football.api-sports.io/leagues")
 
@@ -31,7 +72,7 @@ if response.is_a?(Net::HTTPSuccess)
   json_data = JSON.parse(response.read_body)
 
   if json_data.key?('response')
-    json_data['response'].each do |item|
+    json_data['response'].take(10).each do |item|
       league_data = item['league']
       country_data = item['country']
 
@@ -41,6 +82,7 @@ if response.is_a?(Net::HTTPSuccess)
         custom_id: league_data['id'],
         country_name: country_data['name']
       )
+      get_teams(leagues.custom_id, leagues)
     end
   else
     puts "Key 'response' not found in the JSON data."
@@ -50,37 +92,4 @@ else
 end
 
 puts "There are now #{League.count} leagues in the database."
-
-
-def get_teams(country_name)
-  url = URI("https://v3.football.api-sports.io/leagues?country=#{country_name}")
-
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-request = Net::HTTP::Get.new(url)
-request["x-rapidapi-host"] = 'v3.football.api-sports.io'
-request["x-rapidapi-key"] = '3a1526697e73de9263945ed57a8e6040'
-
-response = http.request(request)
-
-if response.is_a?(Net::HTTPSuccess)
-  json_data = JSON.parse(response.read_body)
-
-  if json_data.key?('response')
-    json_data['response'].each do |item|
-      team_data = item['league']['standings'].flatten
-
-      team_data_name = team_data['team']['name']
-      team_data.each do |team|
-        team = Team.find_or_create_by(
-          team_name: team_data_name,
-          points: team['points'],
-          goal_diff: team['goalsDiff'],
-        )
-      end
-    end
-  end
-end
-end
+puts "There are now #{Team.count} teams in the database."
