5a2113744b7bb95fcd664f122daba0a8b4e939ec
aucchwas <ucchwasa@gmail.com>
2024-10-28 19:22:46 -0500

    scared league is working but team is not might break peace

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ if response.is_a?(Net::HTTPSuccess)
       league_data = item['league']
       country_data = item['country']
 
-      League.find_or_create_by!(
+      leagues = League.find_or_create_by!(
         league_name: league_data['name'],
         league_type: league_data['type'],
         custom_id: league_data['id'],
@@ -50,3 +50,37 @@ else
 end
 
 puts "There are now #{League.count} leagues in the database."
+
+
+def get_teams(country_name)
+  url = URI("https://v3.football.api-sports.io/leagues?country=#{country_name}")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["x-rapidapi-host"] = 'v3.football.api-sports.io'
+request["x-rapidapi-key"] = '3a1526697e73de9263945ed57a8e6040'
+
+response = http.request(request)
+
+if response.is_a?(Net::HTTPSuccess)
+  json_data = JSON.parse(response.read_body)
+
+  if json_data.key?('response')
+    json_data['response'].each do |item|
+      team_data = item['league']['standings'].flatten
+
+      team_data_name = team_data['team']['name']
+      team_data.each do |team|
+        team = Team.find_or_create_by(
+          team_name: team_data_name,
+          points: team['points'],
+          goal_diff: team['goalsDiff'],
+        )
+      end
+    end
+  end
+end
+end
